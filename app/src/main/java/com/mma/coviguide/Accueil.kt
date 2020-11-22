0e2fe8937b31412c3af02ffcf789970dd53d6687
MarwaBD <marwa1998bendaoud@gmail.com>
2020-11-22 10:31:41 +0100

    CoviGuide first commit

---
+++ app/src/main/java/com/mma/coviguide/Accueil.kt
@@ -0,0 +1,17 @@
+package com.mma.coviguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_accueil.*
+
+class Accueil : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil)
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/mma/coviguide/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mma.coviguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutvirus)
+        setContentView(R.layout.activity_aboutvirus)
+    }
+}

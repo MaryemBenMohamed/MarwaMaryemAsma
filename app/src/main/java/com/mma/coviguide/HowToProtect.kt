dec0f65b200f2509b3ca6593c574c248932a5c3c
MaryemBenMohamed <maryoumabac18@gmail.com>
2020-11-22 13:30:44 +0100

    CoviGuid2

---
+++ app/src/main/java/com/mma/coviguide/HowToProtect.kt
@@ -0,0 +1,11 @@
+package com.mma.coviguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HowToProtect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_to_protect)
+    }
+}
